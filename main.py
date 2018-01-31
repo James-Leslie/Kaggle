6603c8946be8c0fc0915bbef1d6a4b15499d9896
Devon Stone <devon12stone@gmail.com>
2018-01-31 14:20:03 +0200

    Repalced NaN values with median in 'Age'

--- main.py
+++ main.py
@@ -8,4 +8,8 @@ df = pd.read_csv('train.csv', index_col=0)
 # drop irrelevant columns
 df = df.drop(['Name', 'Ticket'], axis=1)
 
+#replacing nan values in age
+age_median = df['Age'].median()
+df['Age'].fillna(age_median, inplace=True)
+
 print(df.head())
