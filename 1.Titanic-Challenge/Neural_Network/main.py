75074d415be6e63ab3688a448ecd7f91d75da2b6
James-Leslie <jlesl09@gmail.com>
2018-02-05 21:37:03 +0200

    added house-prices subfolder

--- 1.Titanic-Challenge/Neural_Network/main.py
+++ 1.Titanic-Challenge/Neural_Network/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from pre_process import prepare_data
+from ../pre_process import prepare_data
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 
